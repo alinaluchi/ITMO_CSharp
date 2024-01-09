60014a36d65273f629793423e45cc887d1082136
alinaluchi <149653448+alinaluchi@users.noreply.github.com>
2024-01-09 19:59:11 +0300

    Update and rename AController.cs to Program.cs

---
+++ Lab4/Program.cs
@@ -0,0 +1,33 @@
+using ALab4.DB;
+using ALab4.Repository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("Database"));
+});
+
+
+builder.Services.AddTransient<IARepository, ARepository>();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
